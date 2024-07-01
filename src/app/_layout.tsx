e5c2fbd041f12ca5fdbf06a0c16b17015e0abf0d
rubensmp <rubens.mp1@gmail.com>
2024-07-01 11:48:40 -0300

    chore: config react query + axios

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,17 @@
 import '@/styles/global.css';
 
+import { useEffect } from 'react';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+  focusManager,
+  onlineManager,
+} from '@tanstack/react-query';
+import NetInfo from '@react-native-community/netinfo';
+
 import {
   useFonts,
   Roboto_700Bold,
@@ -10,8 +20,31 @@ import {
 } from '@expo-google-fonts/roboto';
 
 import { Loading } from '@/components/loading';
+import { AppState, AppStateStatus } from 'react-native';
+
+const client = new QueryClient({
+  queryCache: new QueryCache(),
+});
 
 export default function Layout() {
+  useEffect(() => {
+    onlineManager.setEventListener(setOnline => {
+      return NetInfo.addEventListener(state => {
+        setOnline(!!state.isConnected);
+      });
+    });
+  }, [NetInfo, onlineManager]);
+
+  useEffect(() => {
+    const subscriber = AppState.addEventListener('change', onFocusRefetch);
+
+    return () => subscriber.remove();
+  }, []);
+
+  function onFocusRefetch(status: AppStateStatus) {
+    focusManager.setFocused(status === 'active');
+  }
+
   const [fontsLoaded] = useFonts({
     Roboto_700Bold,
     Roboto_500Medium,
@@ -19,9 +52,9 @@ export default function Layout() {
   });
 
   return (
-    <>
+    <QueryClientProvider client={client}>
       <StatusBar style="light" />
       {fontsLoaded ? <Slot /> : <Loading />}
-    </>
+    </QueryClientProvider>
   );
 }
